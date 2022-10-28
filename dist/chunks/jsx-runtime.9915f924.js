1286282ffd6ebfb102755380c5a6e00b064fa4f4
HagaKun <obutora@users.noreply.github.com>
2022-10-28 23:52:47 +0000

    fix : missing link

--- dist/chunks/jsx-runtime.9915f924.js
+++ dist/chunks/jsx-runtime.9915f924.js
@@ -1,4 +1,4 @@
-import{r as i}from"./index.a9b0f316.js";var l={exports:{}},n={};/**
+import{r as i}from"./index.fd924878.js";var l={exports:{}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
--- dist/client.3419c18a.js
+++ dist/client.3419c18a.js
@@ -1,4 +1,4 @@
-import{r as yn}from"./chunks/index.a9b0f316.js";var No={exports:{}},ve={},zo={exports:{}},Po={};/**
+import{r as yn}from"./chunks/index.fd924878.js";var No={exports:{}},ve={},zo={exports:{}},Po={};/**
  * @license React
  * scheduler.production.min.js
  *
